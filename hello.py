67efb0973faf7b7ddcfeb8ed0448a14a5b63d777
pk8753 <khanmohammadjamal@gmail.com>
2019-11-25 23:19:05 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello world")
+N = int(input())
+
+# Get the input
+numArray = list(map(int, input().split()))
+
+sum_integer = 0
+# Write the logic to add these numbers here
+#for i in numArray:
+#    sum_integer += i
+
+for i in range(0,N):
+    sum_integer += numArray[i]
+ 
+# Print the sum
+print(sum_integer)
